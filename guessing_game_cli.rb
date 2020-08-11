4fc237b3bbab1d6f62588f1f46bfd2b633a6e01d
Melinda Diaz <melindadiaz@me.com>
2020-08-11 17:06:06 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,6 +2,13 @@
 def run_guessing_game
   random_number = rand(6) + 1
   input = gets.chomp
-  
+  if input == random_number
+    puts "You guessed the correct number!"
+  elseif input == "exit" 
+    puts "Goodbye!"
+  else 
+    puts "Sorry! The computer guessed #{random_number}."
+    
+  end
   
 end
